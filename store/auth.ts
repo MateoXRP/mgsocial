4764614b7cc565007aba99a1dc5625e7b5ccdfde
MateoXRP <52558401+MateoXRP@users.noreply.github.com>
2021-11-01 22:45:49 -0700

    Add files via upload

--- store/auth.ts
+++ store/auth.ts
@@ -1,9 +1,15 @@
 import { mutationTree } from 'typed-vuex';
 import { User } from '~/composables/useUserDetail';
 
+interface CoilInfo {
+  sub: string;
+  email: string;
+}
+
 export const state = () => ({
   isAuthenticated: false,
   user: null as null | User,
+  coilInfo: null as null | CoilInfo,
 });
 
 export const mutations = mutationTree(state, {
@@ -12,5 +18,8 @@ export const mutations = mutationTree(state, {
   },
   setUser(state, payload: null | User) {
     state.user = payload;
+  },
+  setCoilInfo(state, payload: null | CoilInfo) {
+    state.coilInfo = payload;
   },
 });
