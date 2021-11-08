55601ae94eb5d2d7c214510dd35cdc7d4608ce12
MateoXRP <52558401+MateoXRP@users.noreply.github.com>
2021-11-08 09:36:34 -0800

    Add files via upload

---
+++ pages/administrator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>admin</div>
+</template>
+
+<script lang="ts">
+import { useDashboard } from '~/composables';
+
+export default defineComponent({
+  layout: 'authenticated',
+  middleware: ['auth', 'admin'],
+  setup() {
+    const { data, isLoading } = useDashboard();
+
+    return {
+      data,
+      isLoading,
+    };
+  },
+});
+</script>
