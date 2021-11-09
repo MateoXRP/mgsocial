2a700a6680bab96a90c1d94ba04f2ed6db8c689b
MateoXRP <52558401+MateoXRP@users.noreply.github.com>
2021-11-09 01:27:54 -0800

    Create _id.vue

---
+++ pages/photos/_type/view/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <VCard :loading="isLoading" elevation="1">
+    <VCardTitle>
+      <VSpacer />
+      <VBtn text @click="$router.go(-1)"
+        >Back to {{ $route.params.type }} Photos</VBtn
+      ></VCardTitle
+    >
+    <VCardText>
+      <AppImgWithPlaceholder
+        v-if="data"
+        class="grey lighten-2"
+        :src="data.payload.photo.image_url"
+      />
+    </VCardText>
+  </VCard>
+</template>
+
+<script lang="ts">
+import { usePhotoDetail } from '~/composables';
+import AppImgWithPlaceholder from '~/components/AppImgWithPlaceholder.vue';
+export default defineComponent({
+  components: { AppImgWithPlaceholder },
+  layout: 'authenticated',
+  setup() {
+    const route = useRoute();
+    const { data, isLoading } = usePhotoDetail(route.value.params.id);
+    return {
+      data,
+      isLoading,
+    };
+  },
+});
+</script>
