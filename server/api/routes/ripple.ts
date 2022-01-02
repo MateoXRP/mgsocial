728286da5cec5a17e1a9bfc2b343d3a1ce50146c
MateoXRP <52558401+MateoXRP@users.noreply.github.com>
2022-01-01 21:36:40 -0800

    Add files via upload

--- server/api/routes/ripple.ts
+++ server/api/routes/ripple.ts
@@ -7,20 +7,62 @@ const PUBLIC_SERVER =
   process.env.RIPPLED_PUBLIC_WS_URL || 'wss://xrplcluster.com';
 const client = new Client(PUBLIC_SERVER);
 
+const MGS_ORIGIN_ADDRESS = 'rMgsw2NVqipZZhnVDN9xKuujLAWeL21qFi';
+
 router.get('/ripple/get-trust-lines', async (req, res) => {
   const { address } = req.query;
   await client.connect();
 
-  const data = await client.request({
+  const {
+    result: { lines },
+  } = await client.request({
     command: 'account_lines',
     account: address as string,
     ledger_index: 'validated',
   });
 
   await client.disconnect();
-  const specs = data.result.lines.map((line) => line);
+
+  const specs = lines.map((line) => line);
 
   return res.json(specs);
+});
+
+router.get('/ripple/get-total-rewards', async (req, res) => {
+  const { address } = req.query;
+  await client.connect();
+
+  const {
+    result: { transactions },
+  } = await client.request({
+    command: 'account_tx',
+    account: address as string,
+  });
+
+  const amount = transactions
+    .map(({ tx }) => tx)
+    .filter((tx) => tx && tx.TransactionType === 'Payment')
+    .filter((payment) => payment && payment.Account === MGS_ORIGIN_ADDRESS)
+    .filter(
+      (payment) =>
+        payment &&
+        typeof payment === 'object' &&
+        // @ts-expect-error: Missing types from module
+        payment.Amount.currency === 'MGS'
+    )
+    .reduce(
+      // @ts-expect-error: Missing types from module
+      (previousValue, { Amount: { value: currentValue } }) =>
+        previousValue + parseFloat(currentValue),
+      0
+    );
+
+  // TODO: Disconnect client not working
+  // await client.disconnect();
+
+  return res.json({
+    amount,
+  });
 });
 
 export default router;
